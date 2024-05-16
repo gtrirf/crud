040e223a005827e0fdf88aa62f05c51b019f3ada
iron fury <131025716+gtrirf@users.noreply.github.com>
2024-05-16 23:07:00 +0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('users/', include('foydalanuvchilar.urls')),
     path('books/', include('bookmarket.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
